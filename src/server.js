4032f28a17c15f2e4b48bfb3e7c812f4342e1d16
Guada <guadapdls@gmail.com>
2024-09-09 21:43:01 -0300

    please

--- src/server.js
+++ src/server.js
@@ -3,33 +3,36 @@ import "dotenv/config.js";
 import router from "./routes/index.js";
 import cors from "cors";
 import config from "./config/config.js";
-import http from "http"
+import http from "http";
 import { logger } from "./utils/error/logger.js";
 import errorHandler from "./middlewares/errorHandler.js";
 import notFoundHandler from "./middlewares/notFoundHandler.js";
 
 const server = express();
 
+// Middleware de CORS, configurado correctamente
 server.use(
     cors({
-        origin: '*',
-        methods: 'GET,POST,PUT,DELETE,OPTIONS',
-        allowedHeaders: 'Content-Type'
+        origin: ['https://kingdom-institute-v2.vercel.app'], 
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+        allowedHeaders: ['Content-Type'],
     })
 );
 
 server.get('/favicon.ico', (req, res) => res.status(204));
 
+// Middleware para parsear body de las solicitudes
 server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
 
-server.use('/', router)
-server.options('*', cors());
+// Rutas
+server.use('/', router);
 
-server.use(errorHandler)
-server.use(notFoundHandler)
+// Middlewares de manejo de errores
+server.use(errorHandler);
+server.use(notFoundHandler);
 
-
+// Servidor HTTP
 const httpServer = http.createServer(server);
 
 httpServer.listen(config.PORT, () => {
